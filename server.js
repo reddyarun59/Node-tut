d49058af2e65ef64e85e877963b4b4935873c560
reddyarun59 <reddyarun59@yahoo.com>
2022-04-07 12:13:46 +0530

    render html file trough http

---
+++ server.js
@@ -0,0 +1,20 @@
+const http= require('http');
+const fs= require('fs')
+
+const server=http.createServer((req,res)=>{
+    res.setHeader("Content-Type", "text/html")
+
+    fs.readFile("./views/index.html", (err,data)=>{
+        if(err){
+            console.log(err)
+            res.end()
+        }else{
+            res.write(data)
+            res.end()
+        }
+    })
+})
+
+server.listen(3000, "localhost" , ()=>{
+    console.log("listeningggggg..........")
+})
